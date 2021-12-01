d30931ec68d824c820fc07eca1bdec1349174436
126Florian <91480650+126Florian@users.noreply.github.com>
2021-12-01 16:34:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 #include "lib.h"
-int main(){
+
+int main() {
   int n=0;
   cin>>n;
   if (numeroprimo(n, n-1)){
-      cout<<"numero primo";
+      cout << "numero primo";
   }else{
-        cout<<"numero non primo";
+        cout << "numero non primo";
   }
   return 0;
 }
